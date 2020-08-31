667fea2da1b191070d29236d9c78c6615d28680e
Sean Breckenridge <seanbrecke@gmail.com>
2020-08-31 14:08:25 -0700

    version 0.1.2: improve null handling when serializing

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with io.open("README.md", encoding="utf-8") as fo:
 
 setup(
     name="autotui",
-    version="0.1.1",
+    version="0.1.2",
     url="https://github.com/seanbreckenridge/autotui",
     author="Sean Breckenridge",
     author_email="seanbrecke@gmail.com",
