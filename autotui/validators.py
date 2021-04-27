3b8479f28ad004ba9b8b3b6ab9602a93e8ebceb5
Sean Breckenridge <seanbrecke@gmail.com>
2021-04-27 00:48:58 -0700

    make prompt_toolkit imports lazy

--- autotui/validators.py
+++ autotui/validators.py
@@ -1,21 +1,23 @@
 import sys
 from datetime import datetime
-from typing import Type, Optional, Callable, List, Union, Any
+from typing import Type, Optional, Callable, List, Union, Any, TYPE_CHECKING
+from functools import lru_cache
 
-from prompt_toolkit import prompt
-from prompt_toolkit.validation import Validator, ValidationError
-from prompt_toolkit.shortcuts import button_dialog, input_dialog, message_dialog
-from prompt_toolkit.styles import Style
 
-dark_mode = Style.from_dict(
-    {
-        "dialog": "bg:#000000",
-        "dialog frame.label": "bg:#ffffff #000000",
-        "dialog.body": "bg:#000000 #D3D3D3",
-        "dialog shadow": "bg:#000000",
-        "text-area": "#000000",
-    }
-)
+@lru_cache(maxsize=1)
+def dark_mode():
+
+    from prompt_toolkit.styles import Style
+
+    return Style.from_dict(
+        {
+            "dialog": "bg:#000000",
+            "dialog frame.label": "bg:#ffffff #000000",
+            "dialog.body": "bg:#000000 #D3D3D3",
+            "dialog shadow": "bg:#000000",
+            "text-area": "#000000",
+        }
+    )
 
 
 def create_repl_prompt_str(prompt_msg: str) -> str:
@@ -49,6 +51,8 @@ def handle_prompt(
 
 
 def prompt_str(for_attr: Optional[str] = None, prompt_msg: Optional[str] = None) -> str:
+    from prompt_toolkit import prompt
+
     m: str = handle_prompt(str, for_attr, prompt_msg)
     return prompt(m)
 
@@ -56,37 +60,64 @@ def prompt_str(for_attr: Optional[str] = None, prompt_msg: Optional[str] = None)
 ## INT
 
 
-class IntValidator(Validator):
-    def validate(self, document):
-        text = document.text
-        try:
-            int(text)
-        except ValueError as ve:
-            raise ValidationError(message=str(ve))
+# the Int/Float validators follow this pattern so that prompt_toolkit imports are not
+# at the top level. allows the rest of the library to load fast without waiting
+# for prompt_toolkit to import
+#
+# this does making extending/monkey patching these more annoying, but
+# I think thats not the common case - the common case is waiting on
+# the import while serializing/deserializing the information
+
+
+@lru_cache(maxsize=1)
+def IntValidator() -> Type["Validator"]:
+
+    from prompt_toolkit.validation import Validator, ValidationError
+
+    class _IntValidator(Validator):
+        def validate(self, document):
+            text = document.text
+            try:
+                int(text)
+            except ValueError as ve:
+                raise ValidationError(message=str(ve))
+
+    return _IntValidator
 
 
 def prompt_int(for_attr: Optional[str] = None, prompt_msg: Optional[str] = None) -> int:
+    from prompt_toolkit import prompt
+
     m: str = handle_prompt(int, for_attr, prompt_msg)
-    return int(prompt(m, validator=IntValidator()))
+    return int(prompt(m, validator=IntValidator()()))
 
 
 ## FLOAT
 
 
-class FloatValidator(Validator):
-    def validate(self, document):
-        text = document.text
-        try:
-            float(text)
-        except ValueError as ve:
-            raise ValidationError(message=str(ve))
+@lru_cache(maxsize=1)
+def FloatValidator() -> Type["Validator"]:
+
+    from prompt_toolkit.validation import Validator, ValidationError
+
+    class _FloatValidator(Validator):
+        def validate(self, document):
+            text = document.text
+            try:
+                float(text)
+            except ValueError as ve:
+                raise ValidationError(message=str(ve))
+
+    return _FloatValidator
 
 
 def prompt_float(
     for_attr: Optional[str] = None, prompt_msg: Optional[str] = None
 ) -> float:
+    from prompt_toolkit import prompt
+
     m: str = handle_prompt(float, for_attr, prompt_msg)
-    return float(prompt(m, validator=FloatValidator()))
+    return float(prompt(m, validator=FloatValidator()()))
 
 
 ## BOOL
@@ -97,12 +128,14 @@ def prompt_bool(
     prompt_msg: Optional[str] = None,
     dialog_title: str = "===",
 ) -> bool:
+    from prompt_toolkit.shortcuts import button_dialog
+
     m: str = handle_prompt(bool, for_attr, prompt_msg)
     return button_dialog(
         title=dialog_title,
         text=m,
         buttons=[("True", True), ("False", False)],
-        style=dark_mode,
+        style=dark_mode(),
     ).run()
 
 
@@ -113,12 +146,14 @@ def prompt_datetime(
     for_attr: Optional[str] = None,
     prompt_msg: Optional[str] = None,
 ) -> datetime:
+    from prompt_toolkit.shortcuts import button_dialog, input_dialog, message_dialog
+
     m: str = handle_prompt(datetime, for_attr, prompt_msg)
     use_current_time = button_dialog(
         title="How to pick date?",
         text=m,
         buttons=[("Now", True), ("Describe", False)],
-        style=dark_mode,
+        style=dark_mode(),
     ).run()
     if use_current_time:
         return datetime.now()
@@ -136,7 +171,7 @@ def prompt_datetime(
             time_str: Optional[str] = input_dialog(
                 title="Describe the datetime:",
                 text="For example:\n'2 hours ago', 'noon', 'tomorrow at 10PM', 'may 30th at 8PM'",
-                style=dark_mode,
+                style=dark_mode(),
             ).run()
             if time_str is None:
                 print("Cancelled, using current time...")
@@ -146,7 +181,7 @@ def prompt_datetime(
                 message_dialog(
                     title="Error",
                     text=f"Could not parse '{time_str}' into datetime...",
-                    style=dark_mode,
+                    style=dark_mode(),
                 ).run()
         return parsed_time
 
@@ -159,6 +194,8 @@ def prompt_ask_another(
     prompt_msg: Optional[str] = None,
     dialog_title: str = "===",
 ) -> bool:
+    from prompt_toolkit.shortcuts import button_dialog
+
     m = prompt_msg
     if m is None:
         assert (
@@ -169,7 +206,7 @@ def prompt_ask_another(
         title=dialog_title,
         text=m,
         buttons=[("Yes", True), ("No", False)],
-        style=dark_mode,
+        style=dark_mode(),
     ).run()
 
 
@@ -186,6 +223,8 @@ def prompt_optional(
     A helper to ask if the user wants to enter information for an optional.
     If the user confirms, calls func (which asks the user for input)
     """
+    from prompt_toolkit.shortcuts import button_dialog
+
     m: Optional[str] = prompt_msg
     if m is None:
         assert (
@@ -196,7 +235,7 @@ def prompt_optional(
         title=dialog_title,
         text=m,
         buttons=[("Add", True), ("Skip", False)],
-        style=dark_mode,
+        style=dark_mode(),
     ).run():
         return func()
     else:
@@ -221,6 +260,9 @@ def prompt_wrap_error(
     This is pretty similar to prompt_toolkit.validators.Validation.from_callable
     but it allows you to specify the error message from the callable instead.
     """
+    from prompt_toolkit import prompt
+    from prompt_toolkit.validation import Validator, ValidationError
+
     m: str = handle_prompt(func.__name__, for_attr, prompt_msg)
 
     errors_to_catch: List[Type] = catch_errors or []
@@ -239,3 +281,7 @@ def prompt_wrap_error(
                     raise e
 
     return func(prompt(m, validator=LambdaPromptValidator()))
+
+
+if TYPE_CHECKING:
+    from prompt_toolkit.validation import Validator
--- examples/shortcuts.py
+++ examples/shortcuts.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from datetime import datetime
 from typing import NamedTuple
 
@@ -6,8 +7,16 @@ from autotui.shortcuts import load_prompt_and_writeback
 
 class Water(NamedTuple):
     at: datetime
-    glass_count: float
+    glasses: int
+    per_glass_ml: float
+
+
+
+def main():
+    load_prompt_and_writeback(Water, "~/.local/share/water.json")
+    load_prompt_and_writeback(Water, "~/.local/share/water.json")
+    print(Path("~/.local/share/water.json").expanduser().read_text())
 
 
 if __name__ == "__main__":
-    load_prompt_and_writeback(Water, "~/.local/share/water.json")
+    main()
